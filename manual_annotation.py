a65dd2b0d295e41108e1bcc03c2d9c678c8454d0
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 16:19:31 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -7,9 +7,11 @@ st.write('by Yulei')
 df = pd.read_csv('data2compare.csv')
 import time
 placeholder = st.empty()
+container1 = st.container()
+container2 = st.container()
 for i in range(200):
-    with placeholder.container():
+    with placeholder.container1:
         st.write(df.raw_reviews[i])
-    with placeholder.container():
+    with placeholder.container2:
         st.write(df.processed_reviews[i])
     time.sleep(5)
