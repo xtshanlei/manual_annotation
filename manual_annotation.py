09d9a1b4feb0d389b859c55a0a0140ec90fa1f8d
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 16:24:00 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Mannual Annotation Tool')
+st.title('Manual Annotation Tool')
 st.write('by Yulei')
 
 df = pd.read_csv('data2compare.csv')
 import time
 placeholder = st.empty()
-container1 = st.container()
-container2 = st.container()
+container1 = placeholder.container()
+container2 = placeholder.container()
 for i in range(200):
-    with placeholder.container():
-        container1.write(df.raw_reviews[i])
-        container2.write(df.processed_reviews[i])
-        time.sleep(2)
-        placeholder.empty()
+    container1.write(df.raw_reviews[i])
+    container2.write(df.processed_reviews[i])
+    time.sleep(2)
