f58d1b073f4d5d61667bf3f8078669c81b11989c
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 15:06:38 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -13,5 +13,8 @@ with st.empty():
         st.header('Processed reviews')
         st.write(df.processed_reviews[i])
         st.header('Is the processed reviews what we want?')
-        answer = st.radio('Is the processed reviews what we want?',('YES','NO'))
+        if st.radio('Is the processed reviews what we want?',('YES','NO')) == 'YES':
+            answer = 1
+        else:
+            answer = 0
         st.write(answer)
