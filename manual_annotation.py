5b42c53fde862b64de0c5c79abe4a837ba3b1e3b
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 15:09:00 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -12,3 +12,11 @@ with st.empty():
         st.write(df.raw_reviews[i])
         st.header('Processed reviews')
         st.write(df.processed_reviews[i])
+
+import time
+
+with st.empty():
+    for seconds in range(60):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write("✔️ 1 minute over!")
