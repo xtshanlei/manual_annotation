0987091a3f7c119ffb655e1e660cb7a31a4a4cd7
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 16:10:01 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -6,15 +6,8 @@ st.write('by Yulei')
 
 df = pd.read_csv('data2compare.csv')
 
-container_1 = st.container()
-container_2 = st.container()
-placeholder = st.empty()
-
-import time
-
-for i in range(200):
-    with placeholder.container():
+with st.empty():
+    for i in range(200):
         st.write(df.raw_reviews[i])
-        st.write(df.processed_reviews[i])
-        answer = st.radio('Is the computer doing a good job?',('Yes','No'),key = i)
+        answer = st.radio('asdf',('Y','N'),key =i)
         st.write(answer)
