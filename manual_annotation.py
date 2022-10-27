f5520b45fa60a352ea860d59589f4f39e9ba73cb
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 15:21:34 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -8,16 +8,17 @@ df = pd.read_csv('data2compare.csv')
 
 placeholder = st.empty()
 
+import time
 # Replace the placeholder with some text:
 placeholder.text("Hello")
-
+time.sleep(2)
 # Replace the text with a chart:
 placeholder.line_chart({"data": [1, 5, 2, 6]})
-
+time.sleep(2)
 # Replace the chart with several elements:
 with placeholder.container():
     st.write("This is one element")
     st.write("This is another")
-
+time.sleep(2)
 # Clear all those elements:
 placeholder.empty()
