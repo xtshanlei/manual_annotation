4314302cfed67de93b33810b54ac5d9e4064274d
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 16:30:31 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -5,11 +5,15 @@ st.title('Manual Annotation Tool')
 st.write('by Yulei')
 
 df = pd.read_csv('data2compare.csv')
-import time
-placeholder = st.empty()
-container1 = placeholder.container()
-container2 = placeholder.container()
-for i in range(200):
-    container1.write(df.raw_reviews[i])
-    container2.write(df.processed_reviews[i])
-    time.sleep(2)
+i=0
+while i <=200:
+    st.write(df.raw_review[i])
+    st.write(df.processed_review[i])
+    if st.button('YES'):
+        answer = 1
+    elif st.button('NO'):
+        answer = 0
+
+    if st.button('YES') or st.button('NO'):
+        if st.button('Next'):
+            i +=1
