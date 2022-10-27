51c83364ca3fbf0d6b92add7331d629ac032e85e
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 16:21:21 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -10,8 +10,7 @@ placeholder = st.empty()
 container1 = st.container()
 container2 = st.container()
 for i in range(200):
-    with placeholder.container1:
-        st.write(df.raw_reviews[i])
-    with placeholder.container2:
-        st.write(df.processed_reviews[i])
-    time.sleep(5)
+    with placeholder.container():
+        container1.write(df.raw_reviews[i])
+        container2.write(df.processed_reviews[i])
+    placeholder.empty()
