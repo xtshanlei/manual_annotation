2efd57ed8a9d13d6d2566fcdbca118ed4bc5ee40
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 15:53:42 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -9,9 +9,10 @@ df = pd.read_csv('data2compare.csv')
 container_1 = st.container()
 container_2 = st.container()
 import time
-for i in range(200):
-    container_1.write('Raw reviews:')
-    container_1.write(df.raw_reviews[i])
-    container_2.write('Processed reviews')
-    container_2.write(df.processed_reviews[i])
-    time.sleep(2)
+with st.empty():
+    for i in range(200):
+        container_1.write('Raw reviews:')
+        container_1.write(df.raw_reviews[i])
+        container_2.write('Processed reviews')
+        container_2.write(df.processed_reviews[i])
+        time.sleep(2)
