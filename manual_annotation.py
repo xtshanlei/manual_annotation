951b0d90eecb685c602194f09d3bb3534d650904
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-10-27 14:59:00 +0100

    Update manual_annotation.py

--- manual_annotation.py
+++ manual_annotation.py
@@ -6,10 +6,11 @@ st.write('by Yulei')
 
 df = pd.read_csv('data2compare.csv')
 
-for i in range(200):
-    st.write(df.raw_reviews[i])
-    if st.button('YES'):
-        answer = 1
-    elif st.button('NO'):
-        answer = 0
-    st.write(answer)
+with st.empty():
+    for i in range(200):
+        st.write(df.raw_reviews[i])
+        if st.button('YES'):
+            answer = 1
+        elif st.button('NO'):
+            answer = 0
+        st.write(answer)
